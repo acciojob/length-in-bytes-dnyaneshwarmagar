8875973fc762dc3e4868bcff9273a83837cc03de
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-03-24 20:48:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const byteSize = (str) => {
   // write your code here
-	let blob = new Blob([str]);
-	
-	return blob.length;
+	const encoder = new TextEncoder();
+    // Encode the string to a Uint8Array
+    const bytes = encoder.encode(str);
+    // Return the length of the Uint8Array (size of the string in bytes)
+    return bytes.length;
 };
 
 // Do not change the code below
